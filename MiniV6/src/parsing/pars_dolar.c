1b837e6c3eef23b15960e2c20f9a6cd10a83562f
Maxence Isaac Castiau <misaac-c@c2r13s6.42madrid.com>
2025-01-27 12:05:32 +0100

    Pushing V6

---
+++ MiniV6/src/parsing/pars_dolar.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pars_dolar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 09:22:36 by misaac-c          #+#    #+#             */
+/*   Updated: 2025/01/14 09:24:17 by misaac-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	pars_dolar(char **pars_cmd)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	flag = 0;
+	i = 0;
+	while (pars_cmd[i])
+	{
+		j = 0;
+		if (pars_cmd[i][j] != '$')
+		{
+			if (check_cmd_quotes(pars_cmd[i]) == 0)
+			{
+				if (str_cmp(pars_cmd[i], "|") == 1)
+				{
+					if (i > 1 && str_cmp(pars_cmd[i - 1], "|") == 1)
+						return (0);
+					i++;
+				}
+				else
+					return (0);
+			}
+			else
+				i++;
+		}
+		else
+		{
+			while (pars_cmd[i][j])
+			{
+				if (pars_cmd[i][j] == '$' || pars_cmd[i][j] == '|')
+					j++;
+				else if (((pars_cmd[i][j] >= 48) && (pars_cmd[i][j] <= 57)) || ((pars_cmd[i][j] >= 65) && (pars_cmd[i][j] <= 90)) || ((pars_cmd[i][j] >= 97) && (pars_cmd[i][j] <= 122)) || pars_cmd[i][j] == '_' || pars_cmd[i][j] == '?')
+					j++;
+				else
+					return (0);
+			}
+			i++;
+		}
+	}
+	return (1);
+}
---
+++ MiniV6/src/parsing/pars_syntax.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pars_syntax.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 20:02:17 by misaac-c          #+#    #+#             */
+/*   Updated: 2025/01/16 09:14:37 by misaac-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+static int	len_array(char **pars_cmd)
+{
+	int	i;
+
+	i = 0;
+	while (pars_cmd[i])
+		i++;
+	return (i);
+}
+
+static char	*create_path(char **pars_cmd)
+{
+	char	*tiny_path;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (pars_cmd[i][1] != '\0')
+	{
+		i = 1;
+		tiny_path = malloc(sizeof(char) * ft_strlen(pars_cmd[0]));
+		while (pars_cmd[0][i])
+		{
+			tiny_path[j] = pars_cmd[0][i];
+			j++;
+			i++;
+		}
+		tiny_path[j] = '\0';
+		return (tiny_path);
+	}
+	else
+	{
+		i = 0;
+		tiny_path = malloc(sizeof(char) * ft_strlen(pars_cmd[1]) + 1);
+		while (pars_cmd[1][i])
+		{
+			tiny_path[j] = pars_cmd[1][i];
+			j++;
+			i++;
+		}
+		tiny_path[j] = '\0';
+		return (tiny_path);
+	}
+	return (tiny_path);
+}
+
+int	pars_syntax(char **pars_cmd)
+{
+	char	*tiny_path;
+	int		i;
+	int		len;
+
+	len = len_array(pars_cmd);
+	i = 1;
+	if (pars_cmd[0][0] == '<')
+	{
+		if (pars_cmd[0][i] != '<')
+		{
+			tiny_path = create_path(pars_cmd);
+			if (access(tiny_path, F_OK) < 0)
+			{
+				printf
+				("\033[0;31mMinishell : no such file or directory \033[00m\n");
+				return (free(tiny_path), 0);
+			}
+			free(tiny_path);
+		}
+	}
+	return (1);
+}
